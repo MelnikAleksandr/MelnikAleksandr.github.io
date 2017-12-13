25f3dad79be7eaa79d593d6b0194b8fcdc8d086f
Aleksandr S Melnik <alexmelnik224@gmail.com>
2017-12-13 12:28:36 -0800

    Add files via upload

---
+++ PcBuild/index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+table {
+    font-family: arial, sans-serif;
+    border-collapse: collapse;
+    width: 100%;
+}
+
+td, th {
+    border: 1px solid #dddddd;
+    text-align: left;
+    padding: 8px;
+}
+
+tr:nth-child(even) {
+    background-color: #dddddd;
+}
+</style>
+</head>
+<body>
+
+<table>
+  <tr>
+    <th>Part</th>
+    <th>Image</th>
+    <th>Price</th>
+  </tr>
+  <tr>
+    <td>CPU</td>
+    <td><img src="CPU.png" width='150' height='100'></img></td>
+    <td>$733.26</td>
+  </tr>
+  <tr>
+    <td>CPU Cooler</td>
+    <td><img src="CPU Cooling1.jpg" width='150' height='100'></img><img src="CPU Cooling2.jpg" width='150' height='100'></img></td>
+    <td>$189.58</td>
+  </tr>
+  <tr>
+    <td>Motherboard</td>
+    <td><img src="motherboard.jpg" width='150' height='100'></img></td>
+    <td>$340.98</td>
+  </tr>
+  <tr>
+    <td>Memory</td>
+    <td><img src="Memory.jpg" width='150' height='100'></img></td>
+    <td>$354.40</td>
+  </tr>
+  <tr>
+    <td>Storage</td>
+    <td><img src="SSD.jpg" width='150' height='100'></img><img src="hardrive.jpg" width='150' height='100'></img></td>
+    <td>$399.00</td>
+  </tr>
+  <tr>
+    <td>Video Card</td>
+    <td><img src="gpu.jpg" width='150' height='100'></img></td>
+    <td>$524.99</td>
+  </tr>
+  <tr>
+    <td>Case</td>
+    <td><img src="case.png" width='150' height='100'></img></td>
+    <td>$300.00</td>
+  </tr>
+  <tr>
+    <td>Operating System</td>
+    <td><img src="win10.jpg" width='150' height='100'></img></td>
+    <td>$89.00</td>
+  </tr>
+  <tr>
+    <td>Total Price</td>
+    <td></td>
+    <td>~$3500</td>
+  </tr>
+</table>
+
+</body>
+</html>
